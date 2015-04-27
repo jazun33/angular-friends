baf9bb935b11d9179d478095c47c36848effde7f
jazun33 <jasonmarlow@gmail.com>
2015-04-27 14:48:48 -0600

    I couldn't figure a few things out but it's mostly done.

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,11 @@
+var app = angular.module('angularFriends');
+app.controller('ctrl', function($scope, $http){
 
+    //$scope.friends;
 
+    //import friends-data.json and assign it to $scope.friends
+    $http.get('friend-data.json').then( function(res) {
+            $scope.friends = res.data;
+        });
 
-
-
+});
